f1bbfdfd4c82744898b2b0272323ace7a4dc7c32
Sárdi András <sardi.andras@gmail.com>
2021-04-11 13:56:45 +0200

    Create Hajo.java

---
+++ Hajo.java
@@ -0,0 +1,13 @@
+public class Hajo {
+    private String nev;
+    private int meret;
+    private int agyukSzama;
+    private boolean felavatva;
+
+    public Hajo(){
+        nev = "MARIJAAA";
+        meret = 10;
+        agyukSzama = 2;
+        felavatva = false;
+    }
+}
